754d034b843398966be7c48de5aef041d55e3f92
Maciej Michalski <legeof008@gmail.com>
2022-12-12 21:15:49 +0100

    fix: add more accurate signing method

--- lab9/rsa_server_client/client/z3.py
+++ lab9/rsa_server_client/client/z3.py
@@ -1,17 +1,9 @@
-import hashlib
-
-from Crypto.Hash import SHA512, SHA384, SHA256, SHA, MD5
-from Crypto.Signature import PKCS1_v1_5
 from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
 
 from client import Client
-
-
-def hash_document_and_encrypt_with_private_key(document, key):
-    hash_value = hashlib.sha256(document).hexdigest().encode("utf-8")
-    cipher = PKCS1_OAEP.new(RSA.import_key(key))
-    return cipher.encrypt(hash_value)
 
 
 def encode_document(document, key):
@@ -19,30 +11,21 @@ def encode_document(document, key):
     return cipher.encrypt(document)
 
 
-def verify_signature(signature, sender_pub_key, reciever_private_key):
-    sig_1 = signature.split(b'STOP')[0]
-    sig_2 = signature.split(b'STOP')[1]
-    cipher = PKCS1_OAEP.new(RSA.import_key(reciever_private_key))
-    doc = cipher.decrypt(sig_2)
-    cipher = PKCS1_OAEP.new(RSA.import_key(sender_pub_key))
-    hash_to_be_verified = cipher.decrypt(sig_1)
-    counted_hash = hashlib.sha256(doc).hexdigest().encode("utf-8")
-    if counted_hash == hash_to_be_verified:
-        return True
-    else:
-        return False
-
 def sign(message, priv_key):
-    signer = PKCS1_v1_5.new(priv_key)
+    signer = PKCS1_v1_5.new(RSA.importKey(priv_key))
     digest = SHA256.new()
     digest.update(message)
     return signer.sign(digest)
 
-def verify(message, signature, pub_key):
-    signer = PKCS1_v1_5.new(pub_key)
+
+def verify(message, pub_key, priv_key):
+    encoded_document = message.split(b'STOP')[0]
+    sig = message.split(b'STOP')[1]
+    true_message = PKCS1_OAEP.new(RSA.import_key(priv_key)).decrypt(encoded_document)
+    signer = PKCS1_v1_5.new(RSA.importKey(pub_key))
     digest = SHA256.new()
-    digest.update(message)
-    return signer.verify(digest, signature)
+    digest.update(true_message)
+    return signer.verify(digest, sig)
 
 
 if __name__ == '__main__':
@@ -64,14 +47,17 @@ if __name__ == '__main__':
 
     # Bob's POV
     document = b"ooo"
-    signature_part_1 = hash_document_and_encrypt_with_private_key(document, bob['public'])
-    signature_part_2 = encode_document(document, alice['public'])
-    msg = signature_part_1 + b"STOP" + signature_part_2
+    alices_public_key = c.get_key('alice')
+    encoded_doc = encode_document(document, alices_public_key)
+    signature = sign(document, bob['private'])
+    msg = encoded_doc + b"STOP" + signature
     c.send_binary_message("alice", msg)
 
     # Alice's POV
-    signature = c.get_binary_message("alice")
-    if verify_signature(signature, bob['private'], alice['private']):
-        print("Bob is verified")
+    message = c.get_binary_message("alice")
+    bobs_public_key = c.get_key("bob")
+    print()
+    if verify(message, bobs_public_key, alice['private']):
+        print("Bob is verified by Alice")
     else:
-        print("Bob is not verified")
+        print("Bob could not be verified by Alice")
