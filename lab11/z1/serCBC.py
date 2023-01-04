b77c169a8b0bfc5cfbd8fc1e326886b733bcdac3
Maciej Michalski <mmichalski@catalogicsoftware.com>
2023-01-04 13:44:08 +0100

    fix: rework lab11

--- lab11/z1/serCBC.py
+++ lab11/z1/serCBC.py
@@ -8,75 +8,70 @@ from Crypto.Cipher import DES
 from Crypto.Random import get_random_bytes
 
 
-# zakłada, że a i b są bytes
 def xor64(a, b):
     block = bytearray(a)
-    for j in range(8):
+    for j in range(len(b)):
         block[j] = a[j] ^ b[j]
     return bytes(block)
 
 
 # zakłada, że key i plain_text są bytes
-def encrypt_CBC_serial(key, plain_text):
+def encrypt_CBC_serial(key, plain_text, no_blocks):
     cipher_text = bytearray(plain_text)  # kopia! bytes -> bytearray
-    des = DES.new(key, DES.MODE_CBC)
-    for i in range(no_blocks):
-        offset = i * block_size
-        block = plain_text[offset:offset + block_size]
-        encrypted = des.encrypt(block)
-        cipher_text[offset:offset + block_size] = encrypted
-    return bytes(cipher_text)  # bytearray -> bytes
-
-
-# zakłada, że key i cipher_text są bytes
-def decrypt_CBC_serial(key, cipher_text):
-    plain_text = bytearray(cipher_text)
-    des = DES.new(key, DES.MODE_CBC)
-    for i in range(no_blocks):
-        offset = i * block_size
-        block = cipher_text[offset:offset + block_size]
-        decrypted = des.decrypt(block)
-        plain_text[offset:offset + block_size] = decrypted
-    return bytes(plain_text)
-
-
-def simple_decrypt(idx, cipher_block, block_size):
-    des = DES.new(key, DES.MODE_CBC)
-    decrypted = des.decrypt(cipher_block)
-    for i in range(len(cipher_block)):
-        shared_data[idx * block_size + i] = decrypted[i]
-
-
-def decrypt_CBC_parallel(key, cipher_text, no_blocks):
-    pool = multiprocessing.Pool(no_blocks)
+    des = DES.new(key, DES.MODE_ECB)
     block_size = int(len(cipher_text) / no_blocks)
     blocks = []
     for i in range(no_blocks):
         offset = int(i * block_size)
         block = cipher_text[offset:offset + block_size]
         blocks.append(block)
-    pool.starmap(simple_decrypt, zip(range(no_blocks), blocks, repeat(block_size)))
+    for i in range(no_blocks):
+        if i == 0:
+            block = xor64(blocks[i], iv)
+        else:
+            block = xor64(blocks[i], blocks[i - 1])
+        encrypted = des.encrypt(block)
+        blocks[i] = encrypted
+    return bytes(b"".join(blocks))  # bytearray -> bytes
+
+def simple_decrypt(idx, cipher_block_former, cipher_block_current, block_size):
+    des = DES.new(key, DES.MODE_ECB)
+    decrypted = des.decrypt(cipher_block_current)
+    plain_text = xor64(cipher_block_former, decrypted)
+    for i in range(len(cipher_block_current)):
+        shared_data[idx * block_size + i] = plain_text[i]
+
+
+def decrypt_CBC_parallel(key, cipher_text, no_blocks):
+    pool = multiprocessing.Pool(no_blocks)
+    block_size = int(len(cipher_text) / no_blocks)
+    blocks = []
+    former_blocs = []
+    for i in range(no_blocks):
+        offset = int(i * block_size)
+        block = cipher_text[offset:offset + block_size]
+        if i == 0:
+            former_blocs.append(iv)
+        else:
+            former_blocs.append(blocks[i - 1])
+        blocks.append(block)
+    pool.starmap(simple_decrypt, zip(range(no_blocks), former_blocs, blocks, repeat(block_size)))
 
 
 plain_text = b"alamakot" * 100000
 key = b"haslo123"
-iv = get_random_bytes(8)
-block_size = 8
+block_size = 200000
+iv = get_random_bytes(block_size)
 no_blocks = int(len(plain_text) / block_size)
 
 starttime = time.time()
-cipher_text = encrypt_CBC_serial(key, plain_text)
+cipher_text = encrypt_CBC_serial(key, plain_text, no_blocks)
 print('CBC Encrypt time serial: ', (time.time() - starttime))
-
-starttime = time.time()
-decrypted = decrypt_CBC_serial(key, cipher_text)
-print('CBC Decrypt time serial: ', (time.time() - starttime))
-print('...', decrypted[-15:-1])
 
 # Moja implementacja
 shared_data = multiprocessing.RawArray(ctypes.c_ubyte, plain_text)
 starttime = time.time()
-decrypt_CBC_parallel(key, cipher_text, 4)
+decrypt_CBC_parallel(key, cipher_text, no_blocks)
 endtime = time.time()
 np_arr = numpy.frombuffer(shared_data, dtype=numpy.dtype(shared_data))
 
--- lab11/z2/CRT.py
+++ lab11/z2/CRT.py
@@ -23,57 +23,29 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-# zakłada, że a i b są bytes
 def xor64(a, b):
     block = bytearray(a)
-    for j in range(8):
+    for j in range(len(b)):
         block[j] = a[j] ^ b[j]
     return bytes(block)
 
-
-# zakłada, że key i plain_text są bytes
-def encrypt_CRT_serial(key, plain_text):
-    cipher_text = bytearray(plain_text)  # kopia! bytes -> bytearray
-    ctr = Counter.new(64, initial_value=0, prefix=b"")
-    des = DES.new(key, DES.MODE_CTR, counter=ctr)
-    for i in range(no_blocks):
-        offset = i * block_size
-        block = plain_text[offset:offset + block_size]
-        encrypted = des.encrypt(block)
-        cipher_text[offset:offset + block_size] = encrypted
-    return bytes(cipher_text)  # bytearray -> bytes
-
-
-# zakłada, że key i cipher_text są bytes
-def decrypt_CRT_serial(key, cipher_text):
-    plain_text = bytearray(cipher_text)
-    ctr = Counter.new(64, initial_value=0, prefix=b"")
-    des = DES.new(key, DES.MODE_CTR, counter=ctr)
-    for i in range(no_blocks):
-        offset = i * block_size
-        block = cipher_text[offset:offset + block_size]
-        decrypted = des.decrypt(block)
-        plain_text[offset:offset + block_size] = decrypted
-    return bytes(plain_text)
-
-
-def simple_encrypt(idx, plaintext_block, block_size):
-    ctr = Counter.new(64, initial_value=idx, prefix=b"")
-    des = DES.new(key, DES.MODE_CTR, counter=ctr)
-    decrypted = des.encrypt(plaintext_block)
+def simple_encrypt(idx, plaintext_block, block_size, nonce):
+    des = DES.new(key, DES.MODE_ECB)
+    encrypted = des.encrypt(xor64(nonce, idx.to_bytes(len(nonce),'big')))
+    ciphertext = xor64(encrypted, plaintext_block)
     for i in range(len(plaintext_block)):
-        shared_data_enc[idx * block_size + i] = decrypted[i]
+        shared_data_enc[idx * block_size + i] = ciphertext[i]
 
 
-def simple_decrypt(idx, cipher_block, block_size):
-    ctr = Counter.new(64, initial_value=idx, prefix=b"")
-    des = DES.new(key, DES.MODE_CTR, counter=ctr)
-    decrypted = des.decrypt(cipher_block)
+def simple_decrypt(idx, cipher_block, block_size, nonce):
+    des = DES.new(key, DES.MODE_ECB)
+    decrypted = des.encrypt(xor64(nonce, idx.to_bytes(len(nonce),'big')))
+    plain_text = xor64(cipher_block, decrypted)
     for i in range(len(cipher_block)):
-        shared_data[idx * block_size + i] = decrypted[i]
+        shared_data[idx * block_size + i] = plain_text[i]
 
 
-def encrypt_CRT_parallel(key, plaintext, no_blocks):
+def encrypt_CRT_parallel(nonce, plaintext, no_blocks):
     pool = multiprocessing.Pool(no_blocks)
     block_size = int(len(plaintext) / no_blocks)
     blocks = []
@@ -81,10 +53,10 @@ def encrypt_CRT_parallel(key, plaintext, no_blocks):
         offset = int(i * block_size)
         block = plaintext[offset:offset + block_size]
         blocks.append(block)
-    pool.starmap(simple_encrypt, zip(range(no_blocks), blocks, repeat(block_size)))
+    pool.starmap(simple_encrypt, zip(range(no_blocks), blocks, repeat(block_size), repeat(nonce)))
 
 
-def decrypt_CRT_parallel(key, cipher_text, no_blocks):
+def decrypt_CRT_parallel(nonce, cipher_text, no_blocks):
     pool = multiprocessing.Pool(no_blocks)
     block_size = int(len(cipher_text) / no_blocks)
     blocks = []
@@ -92,15 +64,11 @@ def decrypt_CRT_parallel(key, cipher_text, no_blocks):
         offset = int(i * block_size)
         block = cipher_text[offset:offset + block_size]
         blocks.append(block)
-    pool.starmap(simple_decrypt, zip(range(no_blocks), blocks, repeat(block_size)))
+    pool.starmap(simple_decrypt, zip(range(no_blocks), blocks, repeat(block_size), repeat(nonce)))
 
 
 plain_text = b"alamakot" * 100000
 key = b"haslo123"
-iv = get_random_bytes(200000)
-block_size = 200000
-no_blocks = int(len(plain_text) / block_size)
-
 enc_times = []
 dec_times = []
 
@@ -108,9 +76,12 @@ for threads in [1, 2, 4]:
     print(bcolors.WARNING + "With " + str(threads) + " threads :" + bcolors.ENDC)
     # Moja implementacja szyfrowanie
     shared_data_enc = multiprocessing.RawArray(ctypes.c_ubyte, plain_text)
+    no_blocks = threads
+    block_size = int(len(plain_text) / no_blocks)
+    nonce = get_random_bytes(block_size)
 
     starttime = time.time()
-    encrypt_CRT_parallel(key, plain_text, threads)
+    encrypt_CRT_parallel(nonce, plain_text, threads)
     endtime = time.time()
 
     np_arr = numpy.frombuffer(shared_data_enc, dtype=numpy.dtype(shared_data_enc))
@@ -129,7 +100,7 @@ for threads in [1, 2, 4]:
     shared_data = multiprocessing.RawArray(ctypes.c_ubyte, plain_text)
 
     starttime = time.time()
-    decrypt_CRT_parallel(key, encrypted, threads)
+    decrypt_CRT_parallel(nonce, encrypted, threads)
     endtime = time.time()
 
     np_arr = numpy.frombuffer(shared_data, dtype=numpy.dtype(shared_data))
@@ -139,17 +110,3 @@ for threads in [1, 2, 4]:
     print('CRT decrypt time parallel: ' + bcolors.OKGREEN + str(endtime - starttime) + bcolors.ENDC)
     print('...', parallel_decrypted[-15:-1])
     dec_times.append(endtime - starttime)
-
-barWidth = 0.25
-br1 = np.arange(3)
-br2 = [x + barWidth for x in br1]
-
-plt.bar(br1, enc_times, label="Encryption Times", width=barWidth)
-plt.bar(br2, dec_times, label="Decryption Times", width=barWidth)
-
-plt.xticks([r + barWidth for r in range(3)],
-           ['1', '2', '4'])
-plt.ylabel('Time')
-plt.ylabel('Number of threads')
-plt.legend()
-plt.show()
