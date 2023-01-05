016e8a80ebea875acbe6b5268e5699e33c9cb320
Maciej Michalski <legeof008@gmail.com>
2023-01-05 01:15:37 +0100

    feat: add lab 12

---
+++ lab12/z1/login.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+    int zalogowany;
+    char haslo[8];
+    if (strlen(argv[0]) > 8) {
+        printf("NIE\n");
+        return 0;
+    }
+    zalogowany = 0;
+    strcpy(haslo, argv[1]);
+    if (strcmp(haslo, "Tajne") == 0)
+        zalogowany = 1;
+    if (zalogowany == 1)
+        printf("TAK\n");
+    else
+        printf("NIE\n");
+    return 0;
+}
